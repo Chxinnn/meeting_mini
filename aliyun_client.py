55cac387657577f181ae58f9ec5c645dc80915c3
chxinnn <2973073383@qq.com>
2024-08-29 16:00:59 +0800

    完成接口stop_task和get_result,并完成测试

--- aliyun_client.py
+++ aliyun_client.py
@@ -19,7 +19,10 @@ class AliyunClient:
         request.set_version('2023-09-30')
         request.set_protocol_type('https')
         request.set_uri_pattern('/openapi/tingwu/v2/tasks')
-        request.add_query_param('type', 'realtime')
+        if (status == 'get'):
+            request.set_method('GET')
+            request.set_uri_pattern( '/openapi/tingwu/v2/tasks' + '/' + task_id)
+            return request
         if (status == 'start'):
             body = {
                 "AppKey": self.app_key,
@@ -54,22 +57,44 @@ class AliyunClient:
         request.add_query_param('type', 'realtime')
         request.set_content(json.dumps(body).encode('utf-8'))
         return request
+    
+# TODO 加入异常处理类
 
     def create_task(self, summarization_enabled):
         request = self.build_request(summarization_enabled, '0', 'start')
         response = self.client.do_action_with_exception(request)
         response_json = json.loads(response)
         if response_json['Message'] == 'success':
-            print("创建成功")
-        else:
-            print("创建存在问题" + response_json['Message'])
+            print("创建消息接收成功")
         task_id = response_json['Data']['TaskId']
         meeting_join_url = response_json['Data']['MeetingJoinUrl']
         return task_id, meeting_join_url
 
     def stop_task(self, task_id):
-        pass
+        request = self.build_request(False, task_id, 'stop')
+        response = self.client.do_action_with_exception(request)
+        response_json = json.loads(response)
+        if response_json['Message'] == 'success':
+            print("停止消息接收成功")
+        task_status = response_json['Data']['TaskStatus']
+        return task_status
 
     def get_result(self, task_id):
-        # 实现get_result逻辑
-        return "test_result"
+        request = self.build_request(False, task_id, 'get')
+        response = self.client.do_action_with_exception(request)
+        response_json = json.loads(response)
+        if response_json['Message'] == 'success':
+            print("查询消息接收成功")
+        task_status = response_json['Data']['TaskStatus']
+        if(task_status == 'FAILED'):
+            print('FAILED' + response_json['Data']['ErrorCode'] + response_json['Data']['ErrorMessage'])
+        if(task_status == 'ONGOING' and not response_json['Data']['Result']):
+            print("ONGOING BUT NOT RESULT")
+            return None
+        if(task_status == 'ONGOING' and response_json['Data']['Result']):
+            print("ONGOING AND RESULT")
+            return response_json['Data']['Result']
+        if(task_status == 'COMPLETED'):
+            print("COMPLETED")
+            return response_json['Data']['Result']
+        return None
